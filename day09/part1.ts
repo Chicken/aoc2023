2428dc38e497e97d9b7cea022ef8a8dc6a0f7b51
Antti <antti@antti.codes>
2023-12-09 12:53:10 +0200

    day09

---
+++ day09/part1.ts
@@ -0,0 +1,26 @@
+const values = Deno.readTextFileSync("input")
+    .trim()
+    .split("\n")
+    .map((l) => l.split(" ").map(Number));
+
+let sum = 0;
+
+for (const value of values) {
+    const histories = [value];
+    while (!histories.at(-1)!.every((n) => n === 0)) {
+        const history = histories.at(-1)!;
+        const difference = Array(history.length - 1).fill(0).map((_v, i) => history[i + 1] - history[i]);
+        histories.push(difference);
+    }
+    histories.reverse();
+    for (let i = 0; i < histories.length; i++) {
+        if (i === 0) {
+            histories[i].push(0);
+            continue;
+        }
+        histories[i].push(histories[i].at(-1)! + histories[i - 1]!.at(-1)!);
+    }
+    sum += histories.at(-1)!.at(-1)!;
+}
+
+console.log(sum);
